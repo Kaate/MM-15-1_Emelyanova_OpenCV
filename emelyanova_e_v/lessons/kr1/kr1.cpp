860102689ea7b8ad39b6da23f95dbecc0cae8548
Kate Emelyanova <canory@mail.ru>
2019-04-14 20:09:26 +0300

    add files

--- emelyanova_e_v/lessons/kr1/kr1.cpp
+++ emelyanova_e_v/lessons/kr1/kr1.cpp
@@ -5,28 +5,30 @@ using namespace cv;
 int main()
 {
 
-	Mat image_result(200, 200, CV_8U, Scalar(255));
+	Mat image_result(100, 300, CV_8U, Scalar(255));
+	Mat image_result2(200, 200, CV_8U, Scalar(255));
+	Mat image(200, 300, CV_8U, Scalar(255));
 
-	Mat image_source1(100, 100, CV_8U, Scalar(0));
-	circle(image_source1, Point(50, 50), 30, Scalar(255), -1);
-	image_source1.copyTo(image_result(Rect(0, 100, image_source1.cols, image_source1.rows)));
 
-	Mat image_source2(100, 100, CV_8U, Scalar(255));
-	circle(image_source2, Point(50, 50), 30, Scalar(0), -1);
-	image_source2.copyTo(image_result(Rect(0, 0, image_source2.cols, image_source2.rows)));
+	rectangle(image, Point(0, 0), Point(300/3, 300/3), Scalar(127), -1);
+	circle(image, Point(150/3, 150/3), 30, Scalar(0), -1);
 
-	Mat image_source3(100, 100, CV_8U, Scalar(127));
-	circle(image_source3, Point(50, 50), 30, Scalar(255), -1);
-	image_source3.copyTo(image_result(Rect(100, 0, image_source3.cols, image_source3.rows)));
+	rectangle(image, Point(300 / 3, 0), Point(600 / 3, 300 / 3), Scalar(0), -1);
+	circle(image, Point(450 / 3, 150 / 3), 30, Scalar(255), -1);
 
-	Mat image_source4(100, 100, CV_8U, Scalar(127));
-	circle(image_source4, Point(50, 50), 30, Scalar(0), -1);
-	image_source4.copyTo(image_result(Rect(100, 100, image_source3.cols, image_source3.rows)));
+	rectangle(image, Point(0, 300 / 3), Point(300 / 3, 600 / 3), Scalar(0), -1);
+	circle(image, Point(150 / 3, 450 / 3), 30, Scalar(127), -1);
 
-	Mat image_result1(400, 400, CV_8U, Scalar(255));
-	image_result.copyTo(image_result1(Rect(0, 0, image_result.cols, image_result.rows)));
-	//1
-	Mat dst(200, 200, CV_8U, Scalar(255));
+	rectangle(image, Point(600 / 3, 0), Point(900 / 3, 300 / 3), Scalar(255), -1);
+	circle(image, Point(750 / 3, 150 / 3), 30, Scalar(127), -1);
+
+	rectangle(image, Point(300 / 3, 300 / 3), Point(600 / 3, 600 / 3), Scalar(255), -1);
+	circle(image, Point(450 / 3, 450 / 3), 30, Scalar(0), -1);
+
+	rectangle(image, Point(600 / 3, 300 / 3), Point(900 / 3, 600 / 3), Scalar(127), -1);
+	circle(image, Point(750 / 3, 450 / 3), 30, Scalar(255), -1);
+
+	// FILTER 1
 
 	float kernel1[4];
 	kernel1[0] = 1;
@@ -36,9 +38,10 @@ int main()
 	kernel1[3] = -1;
 
 	Mat kernel_mat1 = Mat(2, 2, CV_32FC1, kernel1);
-	filter2D(image_result, dst, CV_32FC1, kernel_mat1);
-	dst.copyTo(image_result1(Rect(0, 200, dst.cols, dst.rows)));
-	//2
+	filter2D(image, image_result, -1, kernel_mat1, cv::Point(-1, -1), 128, cv::BORDER_REPLICATE);
+
+	// FILTER 2
+
 	Mat dst1(200, 200, CV_8U, Scalar(255));
 
 	float kernel[4];
@@ -50,9 +53,11 @@ int main()
 
 	Mat kernel_mat = Mat(2, 2, CV_32FC1, kernel);
 	filter2D(image_result, dst1, CV_32FC1, kernel_mat);
-	dst1.copyTo(image_result1(Rect(200, 200, dst.cols, dst.rows)));
+	filter2D(image, image_result2, -1, kernel_mat, cv::Point(-1, -1), 128, cv::BORDER_REPLICATE);
 
-	imshow("cv::circle", image_result1);
+	imshow("cv::circle", image);
+	imshow("cv::filter1", image_result);
+	imshow("cv::filter2", image_result2);
 
 	waitKey(0);
 	return 0;
