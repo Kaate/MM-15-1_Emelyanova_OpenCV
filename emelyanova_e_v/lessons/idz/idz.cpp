3346d9bcb9fa6533b77c6857f8d8d54e5cd33d53
Kate Emelyanova <canory@mail.ru>
2019-04-28 22:36:34 +0300

    updated idz

--- emelyanova_e_v/lessons/idz/idz.cpp
+++ emelyanova_e_v/lessons/idz/idz.cpp
@@ -1,10 +1,12 @@
 #include <opencv2/opencv.hpp>
 #include <opencv2/highgui.hpp>
+#include "C:\Program Files\nlohmann_json\single_include\nlohmann\json.hpp"
 
 #include<fstream>
 #include<iostream>
 #include <math.h>
 
+using json = nlohmann::json;
 using namespace cv;
 
 double angle(cv::Point pt1, cv::Point pt2, cv::Point pt0) {
@@ -19,10 +21,7 @@ void find_squares_test(Mat image, std::vector<std::vector<Point> >& squares) {
 	Mat blur(image), canny;
 	GaussianBlur(image, blur, Size(3, 3), 0, 0, BORDER_DEFAULT);
 
-	Canny(blur,            // input image
-		canny,                    // output image
-		30,                        // low threshold
-		200);
+	Canny(blur, canny, 30, 200);
 
 	dilate(canny, canny, Mat(), Point(-1, -1));
 	//imshow("test", canny);
@@ -53,10 +52,31 @@ void find_squares_test(Mat image, std::vector<std::vector<Point> >& squares) {
 	}
 }
 
+double medianMat(cv::Mat Input) {
+	Input = Input.reshape(0, 1); // spread Input Mat to single row
+	std::vector<double> vecFromMat;
+	Input.copyTo(vecFromMat); // Copy Input Mat to vector vecFromMat    
+	std::sort(vecFromMat.begin(), vecFromMat.end()); // sort vecFromMat
+	if (vecFromMat.size() % 2 == 0) { return (vecFromMat[vecFromMat.size() / 2 - 1] + vecFromMat[vecFromMat.size() / 2]) / 2; } // in case of even-numbered matrix
+	return vecFromMat[(vecFromMat.size() - 1) / 2]; // odd-number of elements in matrix
+}
+
 void find_squares(Mat image, std::vector<std::vector<Point> >& squares)
 {
 	Mat blurred(image);
 	medianBlur(image, blurred, 9);
+
+	Mat img_gray(image);
+	cvtColor(img_gray, img_gray, COLOR_BGR2GRAY);
+	double median = medianMat(img_gray);
+	double sigma = 0.33;
+	int lower_thresh = (max(0, (int)((1.0 - sigma) * median)));
+	int upper_thresh = (min(255, (int)((1.0 + sigma) * median)));
+	//std::cout << lower_thresh << "    " << upper_thresh << std::endl;
+
+	double otsu_thresh_val = cv::threshold(
+		img_gray, img_gray, 0, 255, THRESH_BINARY | THRESH_OTSU
+	);
 
 	Mat gray0(blurred.size(), CV_8U), gray;
 	std::vector<std::vector<Point> > contours;
@@ -72,7 +92,9 @@ void find_squares(Mat image, std::vector<std::vector<Point> >& squares)
 
 			if (l == 0)
 			{
-				Canny(gray0, gray, 20, 50, 3); // 10 30
+				//Canny(gray0, gray, lower_thresh, upper_thresh, 3); // 10 30
+				//Canny(gray0, gray, 20, 50, 3); // 10 30
+				Canny(gray0, gray, otsu_thresh_val *0.5, otsu_thresh_val, 3); // 10 30
 
 				dilate(gray, gray, Mat(), Point(-1, -1));
 			}
@@ -118,7 +140,10 @@ double calc_accuracy(std::vector<Point> markup, std::vector<std::vector<cv::Poin
 		cv::RotatedRect minRect = minAreaRect(cv::Mat(squares[j]));
 		cv::Point2f rect_points[4];
 		minRect.points(rect_points);
-		float diagonal = sqrt((rect_points[0].x - rect_points[2].x)* (rect_points[0].x - rect_points[2].x) + (rect_points[0].y - rect_points[2].y)*(rect_points[0].y - rect_points[2].y));
+		float diagonal = sqrt((rect_points[0].x - rect_points[2].x)*
+			(rect_points[0].x - rect_points[2].x) + 
+			(rect_points[0].y - rect_points[2].y)*
+			(rect_points[0].y - rect_points[2].y));
 		if (diagonal > max_dig) {
 			max_dig = diagonal; index_max = j;
 		}
@@ -197,46 +222,97 @@ Mat Gaussian(const Mat &image, std::vector<std::vector<cv::Point> > squares) {
 	return image;
 }
 
-int main( int argc, char** argv ) {
-	//namedWindow("test", WINDOW_AUTOSIZE);
-	Mat tmp;
-	Mat origin;
-	std::vector<std::vector<cv::Point> > squares_on_img; 
-	std::vector<std::vector<cv::Point> > markup;
 
-	
-	if(argc == 1) {
-		std::cout << "Put path to image!" << std::endl;
-		return -1;
-	} else if (argc == 2) {
-	std::cout << argv[1] << std::endl;
+							/*
+
+							 MAIN
+							 MAIN
+
+							*/
+
+
+int main( int argc, char* argv[] ) {
+
+	double accuracy_all = 0.0;
+	std::string path_to_markup = argv[2];
 	std::string path_to_pict = argv[1];
-	origin = imread(path_to_pict);
-	tmp = imread(path_to_pict);
-	//threshold(origin, tmp, 200, 255, THRESH_BINARY);
+
+
+	std::vector<std::string> lst_markup;
+	std::vector<std::string> lst_pict;
+
+	std::ifstream ifs_markup(path_to_markup);
+	if (ifs_markup.is_open())
+	{
+		std::string line;
+		while (getline(ifs_markup, line))
+		{
+			lst_markup.push_back(line);
+		}
+		
 	}
-	/////////////////////// PARSE MART
+	ifs_markup.close();
 
-	if( !origin.data ) { printf("Error loading picture \n"); return -1; }
-
-	find_squares(tmp, squares_on_img);
-	find_squares_test(tmp, squares_on_img);
-	if (squares_on_img.size() == 0) {
-		std::cout << "no quad" << std::endl;
-		std::ofstream out;          // поток для записи
-		out.open("D:/study/emelyanova_e_v/MM-15-1_Emelyanova_OpenCV/emelyanova_e_v/squad.txt"); // окрываем файл для записи
-		out << -1;
-		waitKey(0);
-		return -1;
+	std::ifstream ifs_img(path_to_pict);
+	if (ifs_img.is_open())
+	{
+		std::string line;
+		while (getline(ifs_img, line))
+		{
+			lst_pict.push_back(line);
+		}
 
 	}
-	namedWindow("Counter", WINDOW_AUTOSIZE);
+	ifs_img.close();
+
+	for (int index = 0; index < lst_markup.size(); ++index) {
+
+		double accuracy = 0.0;
+		Mat tmp;
+		Mat origin;
+		std::vector<std::vector<cv::Point> > squares_on_img;
+		std::vector<std::vector<cv::Point> > markup;
+
+		origin = imread(lst_pict[index]);
+		tmp = imread(lst_pict[index]);
+		if (!origin.data) { printf("Error loading picture \n"); return -1; }
+
+		find_squares(tmp, squares_on_img);
+		find_squares_test(tmp, squares_on_img);
+		if (squares_on_img.size() == 0) {
+			std::cout << "no quad" << std::endl;
+			std::ofstream out;
+			out.open("D:/study/emelyanova_e_v/MM-15-1_Emelyanova_OpenCV/emelyanova_e_v/squad.txt"); // окрываем файл для записи
+			out << -1;
+			accuracy = 0.0;
+			//waitKey(0);
+			continue;
+		}
+		//namedWindow("Counter", WINDOW_AUTOSIZE);
+
+		Gaussian(origin, squares_on_img);
+		debugSquares(squares_on_img, origin);
+		//imshow("Counter", origin);
 
 
-	Gaussian(origin, squares_on_img);
-	debugSquares(squares_on_img, origin);
-	imshow("Counter", origin);
+		std::ifstream ifs(lst_markup[index]);
+		json markup_json = json::parse(ifs);
+		std::vector<cv::Point> markup_arr;
+		markup_arr.resize(4);
 
-	waitKey(0);
+		for (int i = 0; i < 4; ++i) {
+			markup_arr[i].x = markup_json["quad"][i][0];
+			markup_arr[i].y = markup_json["quad"][i][1];
+			//std::cout << markup_json["quad"][i] << std::endl;
+		}
+
+		accuracy = calc_accuracy(markup_arr, squares_on_img);
+		std::cout << "accuracy " << index << " = " << accuracy << std::endl;
+		//waitKey(0);
+		accuracy_all += accuracy;
+	}
+
+	std::cout << "accuracy all = " << accuracy_all / lst_markup.size() << std::endl;
+
 	return 0;
 }
